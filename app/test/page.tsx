c1276dde48fea8fb0dafe5a04b94b3cd22398047
Chris Aldred <afluffysquirrel@gmail.com>
2024-09-02 16:46:34 +0000

    fix: screen size overflow

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -36,111 +36,95 @@ export default async function Test() {
           <li></li>
         </ul>
 
-        <main className="flex-col items-stretch min-h-screen justify-between">
-          <div className="z-10 w-full h-full items-center justify-between">
-            <Transition show={true} appear={true}>
-              <div
-                className={`h-full border-slate-100  grid grid-cols-4 grid-rows-8 gap-4 py-5 px-40`}
-              >
-                <FadeIn delay="delay-[0ms]">
-                  <div
-                    className={`text-[16px] font-[150] col-span-3 row-span-4 ${BOX_STYLE}`}
-                  >
-                    Hi, I'm <b>Chris Aldred</b>, a software developer with
-                    strong focus on the user experience, animations and micro
-                    interactions. Feel free to reach out to me if you have any
-                    projects in mind, or just to say hello.
-                  </div>
-                </FadeIn>
+        <main className="flex-col justify-between">
+          <Transition show={true} appear={true}>
+            <div
+              className={`min-h-screen grid grid-cols-4 grid-rows-8 gap-4 py-5 px-52`}
+            >
+              <FadeIn delay="delay-[0ms]">
+                <div
+                  className={`text-[16px] font-[150] col-span-3 row-span-4 ${BOX_STYLE}`}
+                >
+                  Hi, I'm <b>Chris Aldred</b>, a software developer with strong
+                  focus on the user experience, animations and micro
+                  interactions. Feel free to reach out to me if you have any
+                  projects in mind, or just to say hello.
+                </div>
+              </FadeIn>
 
-                <FadeIn delay="delay-[100ms]">
-                  <div
-                    className={`text-sm font-[150] row-span-6 col-start-4 ${BOX_STYLE}`}
-                  >
-                    <h1 className="text-lg font-medium">About me</h1>
-                    Hi, I'm Chris, a full-stack software developer from the UK
-                    🇬🇧
-                    <br />
-                    <br />
-                    My tools of choice are:
-                    <ul>
-                      <li>&#8226; JavaScript/TypeScript</li>
-                      <li>&#8226; React</li>
-                      <li>&#8226; NextJS</li>
-                      <li>&#8226; Tailwind</li>
-                      <li>&#8226; Tina CMS</li>
-                    </ul>
-                    <br />
-                    Beyond coding, I'm passionate about rock climbing, music and
-                    traveling. While I have some preferred tools, I always
-                    choose the best one for the job, even if it's not on my
-                    usual list. My goal is to find the right solution for each
-                    project.
-                  </div>
-                </FadeIn>
+              <FadeIn delay="delay-[100ms]">
+                <div
+                  className={`text-sm font-[150] row-span-6 col-start-4 ${BOX_STYLE}`}
+                >
+                  <h1 className="text-lg font-medium">About me</h1>
+                  Hi, I'm Chris, a full-stack software developer from the UK 🇬🇧
+                  <br />
+                  <br />
+                  My tools of choice are:
+                  <ul>
+                    <li>&#8226; JavaScript/TypeScript</li>
+                    <li>&#8226; React</li>
+                    <li>&#8226; NextJS</li>
+                    <li>&#8226; Tailwind</li>
+                    <li>&#8226; Tina CMS</li>
+                  </ul>
+                  <br />
+                  Beyond coding, I'm passionate about rock climbing, music and
+                  traveling. While I have some preferred tools, I always choose
+                  the best one for the job, even if it's not on my usual list.
+                  My goal is to find the right solution for each project.
+                </div>
+              </FadeIn>
 
-                <FadeIn delay="delay-[300ms]">
-                  <div className={`col-start-2 row-start-5 ${BOX_STYLE}`}>
-                    3
-                  </div>
-                </FadeIn>
+              <FadeIn delay="delay-[300ms]">
+                <div className={`col-start-2 row-start-5 ${BOX_STYLE}`}>3</div>
+              </FadeIn>
 
-                <FadeIn delay="delay-[400ms]">
-                  <div className={`col-start-3 row-start-5 ${BOX_STYLE}`}>
-                    4
-                  </div>
-                </FadeIn>
+              <FadeIn delay="delay-[400ms]">
+                <div className={`col-start-3 row-start-5 ${BOX_STYLE}`}>4</div>
+              </FadeIn>
 
-                <FadeIn delay="delay-[200ms]">
-                  <div
-                    className={`row-span-4 col-start-1 row-start-5 ${BOX_STYLE}`}
-                  >
-                    5
-                  </div>
-                </FadeIn>
+              <FadeIn delay="delay-[200ms]">
+                <div
+                  className={`row-span-4 col-start-1 row-start-5 ${BOX_STYLE}`}
+                >
+                  5
+                </div>
+              </FadeIn>
 
-                <FadeIn delay="delay-[500ms]">
-                  <div
-                    className={`row-span-2 col-start-2 row-start-6 ${BOX_STYLE}`}
-                  >
-                    6
-                  </div>
-                </FadeIn>
+              <FadeIn delay="delay-[500ms]">
+                <div
+                  className={`row-span-2 col-start-2 row-start-6 ${BOX_STYLE}`}
+                >
+                  6
+                </div>
+              </FadeIn>
 
-                <FadeIn delay="delay-[600ms]">
-                  <div
-                    className={`row-span-2 col-start-3 row-start-6 ${BOX_STYLE}`}
-                  >
-                    7
-                  </div>
-                </FadeIn>
+              <FadeIn delay="delay-[600ms]">
+                <div
+                  className={`row-span-2 col-start-3 row-start-6 ${BOX_STYLE}`}
+                >
+                  7
+                </div>
+              </FadeIn>
 
-                <FadeIn delay="delay-[1000ms]">
-                  <div className={`col-start-4 row-start-8 ${BOX_STYLE}`}>
-                    8
-                  </div>
-                </FadeIn>
+              <FadeIn delay="delay-[1000ms]">
+                <div className={`col-start-4 row-start-8 ${BOX_STYLE}`}>8</div>
+              </FadeIn>
 
-                <FadeIn delay="delay-[700ms]">
-                  <div className={`col-start-4 row-start-7 ${BOX_STYLE}`}>
-                    9
-                  </div>
-                </FadeIn>
+              <FadeIn delay="delay-[700ms]">
+                <div className={`col-start-4 row-start-7 ${BOX_STYLE}`}>9</div>
+              </FadeIn>
 
-                <FadeIn delay="delay-[800ms]">
-                  <div className={`col-start-2 row-start-8 ${BOX_STYLE}`}>
-                    10
-                  </div>
-                </FadeIn>
+              <FadeIn delay="delay-[800ms]">
+                <div className={`col-start-2 row-start-8 ${BOX_STYLE}`}>10</div>
+              </FadeIn>
 
-                <FadeIn delay="delay-[900ms]">
-                  <div className={`col-start-3 row-start-8 ${BOX_STYLE}`}>
-                    11
-                  </div>
-                </FadeIn>
-              </div>
-            </Transition>
-          </div>
+              <FadeIn delay="delay-[900ms]">
+                <div className={`col-start-3 row-start-8 ${BOX_STYLE}`}>11</div>
+              </FadeIn>
+            </div>
+          </Transition>
         </main>
       </div>
     </>
