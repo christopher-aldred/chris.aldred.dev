2e8ea05e867d9051ce38d08a74ceb161e820248b
Chris Aldred <afluffysquirrel@gmail.com>
2024-09-03 15:43:59 +0000

    chore: viewport config

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,6 @@ import type { Viewport } from "next";
 
 export const viewport: Viewport = {
   width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
   viewportFit: "cover",
 };
 
