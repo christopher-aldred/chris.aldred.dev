9448cf2147910ef563644ead205aefb0fd6d1e47
Chris Aldred <afluffysquirrel@gmail.com>
2024-09-03 14:35:36 +0000

    fix: viewport meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -16,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
