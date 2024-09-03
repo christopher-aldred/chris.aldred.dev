a1a544b528edc25c998c850f67a9d2ee50d0047d
Chris Aldred <afluffysquirrel@gmail.com>
2024-09-03 14:31:44 +0000

    fix: viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Chris Aldred",
-  description: "A Next.js app with TinaCMS",
+  description: "A developer portfolio site",
+  viewportFit: "cover",
 };
 
 export default function RootLayout({
@@ -15,10 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, viewport-fit=cover"
-      ></meta>
       <body className={inter.className}>{children}</body>
     </html>
   );
