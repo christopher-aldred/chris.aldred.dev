5c1e19b3cef254877e9d1e904cffc5218dbd6161
Chris Aldred <afluffysquirrel@gmail.com>
2024-09-08 22:19:42 +0000

    fix: notch color

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,10 @@ export const viewport: Viewport = {
   maximumScale: 1,
   userScalable: false,
   viewportFit: "cover",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#131313" },
+    { media: "(prefers-color-scheme: dark)", color: "#131313" },
+  ],
 };
 
 const inter = Inter({ subsets: ["latin"] });
