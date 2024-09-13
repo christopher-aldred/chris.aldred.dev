be0744318768ca6ba659e94960d00688ea28d558
Chris Aldred <afluffysquirrel@gmail.com>
2024-09-13 18:32:16 +0000

    chore: make root page

--- components/bookingModal.tsx
+++ components/bookingModal.tsx
@@ -51,7 +51,7 @@ export default function BookingModal() {
                     height="650px"
                   ></iframe>
                   <Link
-                    href="/test"
+                    href="/"
                     className="mt-4 px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
                   >
                     Close
