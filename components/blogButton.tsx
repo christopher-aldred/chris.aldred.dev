eafea7d52c5f66e6f04a3df1a297748371f84417
Chris Aldred <afluffysquirrel@gmail.com>
2024-09-14 18:38:48 +0000

    fix: blog buttton icon

--- components/blogButton.tsx
+++ components/blogButton.tsx
@@ -12,8 +12,24 @@ export default function BlogButton() {
           objectFit="cover"
           alt="Picture of the author"
         />
-        <div className="text-2xl font-[100] absolute inset-y-0 left-0 z-30 w-full rounded-lg bg-gradient-to-tl from-transparent to-neutral-900/95 flex">
-          <div className="m-auto">Blog / News</div>
+        <div className="text-xl lg:text-2xl font-[100] absolute inset-y-0 left-0 z-30 w-full rounded-lg bg-gradient-to-tl from-transparent to-neutral-900/95 flex">
+          <div className="m-auto flex justify-center items-center">
+            Blog&nbsp;
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+              />
+            </svg>
+          </div>
         </div>
       </div>
     </FadeAndSlide>
