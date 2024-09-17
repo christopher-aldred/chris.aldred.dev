eb511f4e1de036287d99ed4554e673b15f244a98
Chris Aldred <afluffysquirrel@gmail.com>
2024-09-17 13:29:41 +0000

    Revert "feat: better screen fit"
    This reverts commit 1019dd2ed1b29a241ab785472edee26a7fa1d30c.

--- app/page.tsx
+++ app/page.tsx
@@ -25,18 +25,18 @@ export default function Portfolio({ searchParams }: SearchParamProps) {
       </Transition>
 
       <div className="z-10 area flex">
-        <main className="justify-between flex flex-col min-h-screen">
+        <main className="justify-between overflow-scroll lg:overflow-hidden">
           <Transition show={true} appear={true}>
             <FadeAndSlide delay="delay-[0ms]">
               <header className="flex-shrink-0">
-                <h1 className="py-2 lg:py-4 px-2 lg:px-4 xl:px-36 font-[100] text-2xl lg:text-4xl drop-shadow-[5px_5px_7px_rgba(0,0,0,0.99)] lg:drop-shadow-[5px_5px_10px_rgba(0,0,0,0.99)] text-transparent bg-clip-text bg-white">
+                <h1 className="p-2 mt-2 lg:mt-4 lg:gap-4 lg:p-4 xl:px-36 font-[100] text-2xl lg:text-4xl drop-shadow-[5px_5px_7px_rgba(0,0,0,0.99)] lg:drop-shadow-[5px_5px_10px_rgba(0,0,0,0.99)] text-transparent bg-clip-text bg-white">
                   <div className="font-[250]">Chris Aldred</div>
                   Development & Freelancing
                 </h1>
               </header>
             </FadeAndSlide>
             <div
-              className={`max-h-full overflow-auto grid grid-cols-4 lg:grid-rows-7 gap-2 p-2 lg:gap-4 lg:p-4 xl:px-36`}
+              className={`h-[650px] grid grid-cols-4 lg:grid-rows-7 gap-2 p-2 lg:gap-4 lg:p-4 xl:px-36`}
             >
               <MainComponent />
 
--- components/blogButton.tsx
+++ components/blogButton.tsx
@@ -4,16 +4,16 @@ import Image from "next/image";
 export default function BlogButton() {
   return (
     <FadeAndSlide delay="delay-[500ms]">
-      <div className="opacity-90 flex flex-col col-span-2 lg:col-span-1  items-center justify-center custom-box-style relative">
+      <div className="flex flex-col col-span-2 lg:col-span-1  items-center justify-center custom-box-style relative">
         <Image
-          className="opacity-20 rounded-lg object-cover"
+          className="opacity-10 rounded-lg object-cover"
           src="/blogging.jpg"
           layout="fill"
           objectFit="cover"
           alt="Picture of the author"
           priority
         />
-        <div className="text-xl lg:text-2xl font-[100] absolute inset-y-0 left-0 z-30 w-full rounded-lg bg-gradient-to-tl from-transparent to-neutral-950 flex">
+        <div className="text-xl lg:text-2xl font-[100] absolute inset-y-0 left-0 z-30 w-full rounded-lg bg-gradient-to-tl from-transparent to-neutral-900/95 flex">
           <div className="m-auto flex justify-center items-center">
             Blog&nbsp;
             <svg
