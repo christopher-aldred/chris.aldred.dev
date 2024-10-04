595fa60009538a6bf711bcbc765cd734c6f4317b
afluffysquirrel <afluffysquirrel@gmail.com>
2024-10-04 16:11:11 +0100

    fix: modal dvh

--- components/fullScreenModal.tsx
+++ components/fullScreenModal.tsx
@@ -35,7 +35,7 @@ export default function FullScreenModal() {
       <FadeIn delay="delay-[100ms]">
         <div className="z-50 fixed inset-0 bg-black/70 h-full w-full flex items-center justify-center">
           <FadeAndSlide delay="delay-[300ms]">
-            <div className="p-4 border border-neutral-700 shadow-lg rounded-lg bg-neutral-600/50 backdrop-blur-lg flex flex-col max-h-[90svh] w-[90vw] lg:w-[50vw]">
+            <div className="p-4 border border-neutral-700 shadow-lg rounded-lg bg-neutral-600/50 backdrop-blur-lg flex flex-col max-h-[90dvh] w-[90vw] lg:w-[50vw]">
               <div className="flex flex-col">
                 <div className="text-center text-neutral-100 font-[200] text-2xl lg:text-4xl mb-4">
                   Portfolio
